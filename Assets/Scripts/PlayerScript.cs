4e2654abf642bc7f0e733ddcbbcf5468612f0012
Wojtek <wojtekchmielewski586@gmail.com>
2026-01-31 15:12:32 +0000

    Mouse Aim and Scoping in

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,30 +1,42 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Cinemachine;
 
 public class PlayerScript : MonoBehaviour
 {
     public GameObject m_ScopeOverlay;
     public GameObject m_Scope;
+    public CinemachineCamera m_UnscopedCamera;
+    public CinemachineCamera m_ScopedCamera;
     public Camera m_Camera;
+    public PlayerInput m_input;
+    InputAction m_Action;
 
     //Camera Scope values
     const float CAMERA_UNSCOPED_SIZE = 5.4f;
     const float CAMERA_SCOPED_SIZE = 1.5f;
     bool m_scopedIn = false;
-    float m_cameraCurrentSize = 0;
-    float m_cameraTargetSize = 0;
+    Vector3 m_Mousepos = Vector3.zero;
+    [Range(0.005f, 0.1f)]
+    public float m_UnscopedSensitivity = 0.05f;
+    [Range(0.005f, 0.1f)]
+    public float m_ScopedSensitivty = 0.05f;
+
+    Vector3 delta;
 
     void Start()
     {
-        m_cameraCurrentSize = CAMERA_UNSCOPED_SIZE;
-        m_cameraTargetSize = CAMERA_UNSCOPED_SIZE;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        m_Action = m_input.actions.FindAction("Look");
+
     }
 
 
     void Update()
     {
-        CameraUpdate();
+        MouseAim();
     }
 
     public void ZoomIn(InputAction.CallbackContext context)
@@ -32,43 +44,40 @@ public class PlayerScript : MonoBehaviour
         if (context.started)
         {
             Debug.Log("Zoomed in");
-            m_cameraTargetSize = CAMERA_SCOPED_SIZE;
             m_ScopeOverlay.SetActive(true);
             m_scopedIn = true;
-
-
 
         }
 
         if (context.canceled)
         {
             Debug.Log("Zoom out");
-            m_cameraTargetSize = CAMERA_UNSCOPED_SIZE;
             m_ScopeOverlay.SetActive(false);
             m_scopedIn = false;
         }
+        m_UnscopedCamera.gameObject.SetActive(!m_scopedIn);
+        m_ScopedCamera.gameObject.SetActive(m_scopedIn);
 
     }
 
-    public void MouseAim(InputAction.CallbackContext context)
+    public void MouseAim()
     {
-        Vector2 pos = context.ReadValue<Vector2>();
-        Vector2 worldPos = m_Camera.ScreenToWorldPoint(pos);
-        m_Scope.transform.position = worldPos;
+
+        delta = m_Action.ReadValue<Vector2>();
         if (m_scopedIn)
         {
-            m_Camera.gameObject.transform.position = worldPos;
+            delta *= m_ScopedSensitivty;
+            m_Mousepos += delta;
         }
-        Debug.Log(worldPos);
-    }
-
-
-    void CameraUpdate()
-    {
-        if (m_cameraCurrentSize != m_cameraTargetSize)
+        else
         {
-            m_cameraCurrentSize = Mathf.Lerp(m_cameraCurrentSize, m_cameraTargetSize, 0.05f);
-            m_Camera.orthographicSize = m_cameraCurrentSize;
+            delta *= m_UnscopedSensitivity;
+            m_Mousepos += delta;
         }
+
+        m_Mousepos.x = Mathf.Clamp(m_Mousepos.x, -9.6f, 9.6f);
+        m_Mousepos.y = Mathf.Clamp(m_Mousepos.y, -5.4f, 5.4f);
+        m_Scope.transform.position = m_Mousepos;
+        //Debug.Log(delta);
     }
 }
