6c7fcefe09fabf9be5088037b8a5d4c037701e36
Charlie Blaise <Charlie.Blaise@tidygames.co.uk>
2026-01-31 15:17:40 +0000

    Better AI behaviours with spawns and doors

--- Assets/Scripts/AI/SpawnManager.cs
+++ Assets/Scripts/AI/SpawnManager.cs
@@ -1,40 +1,70 @@
 using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SpawnManager : MonoBehaviour
 {
 	public List<Collider2D> m_PatrolAreas = new();
+	public List<Collider2D> m_SpawnAreas = new();
+
 	public GameObject m_AgentPrefab;
 	public int m_MaxAgents;
 
 	PatrolManager2D m_PatrolManager;
 
-	void Start()
+	[SerializeField] InputAction.CallbackContext m_CallbackSpace;
+
+	void Awake()
 	{
 		m_PatrolManager = GetComponent<PatrolManager2D>();
 
-		for ( int i = 0; i <= m_MaxAgents; i++ )
+		for ( int i = 0; i < m_MaxAgents; i++ )
 		{
-			InstantSpawn();
+			DoorSpawn();
 		}
 	}
 
-	void Update()
+	void LateUpdate()
 	{
-		
+		if ( m_PatrolManager.m_Agents.Count < m_MaxAgents )
+		{
+			DoorSpawn();
+		}
 	}
 
-	void InstantSpawn()
+	PatrolAgent2D InstantSpawn( Vector2 xSpawnPosition )
 	{
-		GameObject xNewAgent = Instantiate( m_AgentPrefab );
-		PatrolAgent2D xAgentPatrol = m_AgentPrefab.GetComponent<PatrolAgent2D>();
-		xAgentPatrol.m_PatrolArea = m_PatrolAreas[ Random.Range( 0, m_PatrolAreas.Count ) ];
+		GameObject xNewAgent = Instantiate( m_AgentPrefab, xSpawnPosition, Quaternion.identity, this.transform );
+		PatrolAgent2D xAgentPatrol = xNewAgent.GetComponent<PatrolAgent2D>();
+		int iPatrolAreaIndex = Random.Range( 0, m_PatrolAreas.Count );
+		xAgentPatrol.m_PatrolArea = m_PatrolAreas[ iPatrolAreaIndex ];
 
-		m_PatrolManager.AddAgent( xNewAgent );
+		// Double check that the agent is assigned a patrol area
+		if ( xAgentPatrol.m_PatrolArea == null )
+		{
+			xAgentPatrol.m_PatrolArea = m_PatrolAreas[ 0 ];
+		}
+
+		m_PatrolManager.AddAgent( xAgentPatrol );
+
+		return xAgentPatrol;
 	}
 
-	void NaturalSpawn( int iNumberToSpawn )
+	void DoorSpawn()
 	{
+		PatrolAgent2D xAgent = InstantSpawn( m_SpawnAreas[ Random.Range( 0, m_SpawnAreas.Count ) ].transform.position );
+		xAgent.m_DoorToLeaveFrom = m_SpawnAreas[ Random.Range( 0, m_SpawnAreas.Count ) ].transform.position;
+	}
 
+	void StartDoorDespawn( PatrolAgent2D xDespawningAgent )
+	{
+		xDespawningAgent.DoorDespawn( m_SpawnAreas[ Random.Range( 0, m_SpawnAreas.Count ) ].transform.position );
+	}
+
+	public void OnJump()
+	{
+		StartDoorDespawn( m_PatrolManager.m_Agents[ Random.Range( 0, m_PatrolManager.m_Agents.Count ) ] );
 	}
 }
