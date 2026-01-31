08cfb7d204da66eb7812895482932edae1b9bdd0
SoloMorris <solommorrison@gmail.com>
2026-01-31 18:23:29 +0000

    ditto

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,7 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
+
 namespace DefaultNamespace
 {
-    public class GameController
+    public class GameController : MonoBehaviour
     {
+        [SerializeField] private float roundTimerMin;
+        [SerializeField] private float roundTimerMax;
+        private float roundTimerLimit;
+        public float RoundTimerLimit => roundTimerLimit;
+        private float roundTimer;
+        public float RoundTimer => roundTimer;
         
+        // Singleton instance
+        public static GameController Instance;
+        public UnityAction acquireTargetEvent;
+        
+        
+        private void Awake() {
+            // Ensure singleton instance
+            if (Instance == null) {
+                Instance = this;
+            }
+            else {
+                Destroy(gameObject);  // Ensure only one UIManager exists
+            }
+        }
+
+        private void Start()
+        {
+            acquireTargetEvent += OnFindNewTarget;
+            acquireTargetEvent?.Invoke();
+        }
+
+        private void OnFindNewTarget()
+        {
+            roundTimerLimit = Random.Range(roundTimerMin, roundTimerMax);
+            roundTimer = roundTimerLimit;
+        }
     }
 }
--- Assets/Scripts/UI/WinPanel.cs
+++ Assets/Scripts/UI/WinPanel.cs
@@ -1,7 +1,38 @@
+using UnityEngine.UIElements;
+
 namespace UI
 {
-    public class WinPanel
+    public class GameOverPanel : UiPanel
     {
-        
+        public Button retryButton;
+        public Button quitButton;
+
+        public override void OnShow()
+        {
+            base.OnShow();
+            retryButton = uiObject.Q<Button>("Button_PlayAgain");
+            retryButton.clickable.clicked += OnRetryButtonClicked;  
+            quitButton = uiObject.Q<Button>("Button_Quit");
+            quitButton.clickable.clicked += OnQuitButtonClicked;
+        }
+
+        public override void OnHide()
+        {
+            base.OnHide();
+            retryButton.clickable.clicked += OnRetryButtonClicked;  
+            quitButton.clickable.clicked += OnQuitButtonClicked;
+        }
+
+        private void OnRetryButtonClicked()
+        {
+            HidePanel();
+            //  Restart level
+        }
+
+        private void OnQuitButtonClicked()
+        {
+            HidePanel();
+            //  Load to menu
+        }
     }
 }
